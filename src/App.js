f3b5f8349a05426fe5401c5d9102a6673293c181
Deepankur Lohiya <ankurlohiya3@gmail.com>
2021-05-26 14:56:48 +0530

    google sign in setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React,{useState} from 'react';
+import React from 'react';
 import './App.css';
 import Sidebar from './Sidebar';
 import Chat from './Chat'
 import Login from './Login'
 import { Route,BrowserRouter as Router, Switch } from 'react-router-dom'; 
+import { useStateValue } from './StateProvider';
 function App() {
-  const [user, setUser]= useState(null);
+  const [{ user},dispatch] = useStateValue();
   return (
     <div className="app">
     {!user ? (
